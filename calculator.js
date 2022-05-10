669e7951d6b8e4366813884a6c904c6f0df8535e
Bialcizq <bialiczq@gmail.com>
2022-05-10 19:24:25 +0200

    test

--- calculator.js
+++ calculator.js
@@ -3,6 +3,8 @@ const buttonValues = document.querySelectorAll('.button')
 const firstScreenResult = document.querySelector('.firstScreen');
 const secondScreenResult = document.querySelector('.secondScreen');
 
+console.log('test');
+
 
 
 buttonValues.forEach(button=>{
