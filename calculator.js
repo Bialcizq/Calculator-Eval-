910abcc0d13efdfef2f3b33744605e33d0c6b113
Bialcizq <bialiczq@gmail.com>
2022-05-10 19:20:03 +0200

    create Files

---
+++ calculator.js
@@ -0,0 +1,10 @@
+const buttonValues = document.querySelectorAll('.button')
+
+const firstScreenResult = document.querySelector('.firstScreen');
+const secondScreenResult = document.querySelector('.secondScreen');
+
+
+
+buttonValues.forEach(button=>{
+    button.addEventListener('click', ()=> {})})
+        
